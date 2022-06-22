6f0f929115b0896f231838c6ecc2170878239eda
raydover <105178034+raydover@users.noreply.github.com>
2022-06-22 15:59:42 -0400

    add readAndAppend, writeToFile server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -24,10 +28,45 @@ app.get('/notes', (req, res) =>
 
 // GET Route for retrieving all the Notes
 app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request received for tips`);
+  console.info(`${req.method} request received for notes`);
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST Route for a new Note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { note } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      note,
+    };
+
+    readAndAppend(newNotes, './db/notes.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+// 
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
