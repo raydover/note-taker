59938e9dd5105463267fa31115218ece1ba55c24
raydover <105178034+raydover@users.noreply.github.com>
2022-06-22 15:35:57 -0400

    get route retieve all notes, server.js

--- server.js
+++ server.js
@@ -17,10 +17,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public.index.html'));
 });
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
+// GET Route for Notes page
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET Route for retrieving all the Notes
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
 app.listen(PORT, () =>
