8773fb9ef8b06262217f7d3a0b7186f3d0638be0
raydover <105178034+raydover@users.noreply.github.com>
2022-06-22 15:29:40 -0400

    comments to JS, add GET notes

--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// GET Route from Homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public.index.html'));
+});
 
-
-
-
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 app.listen(PORT, () =>
